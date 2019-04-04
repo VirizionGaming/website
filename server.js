3825e82c0ca895c2f563cae7aaa7e679d9256dba
VirizionNetworks <43611960+VirizionNetworks@users.noreply.github.com>
2019-04-04 14:23:55 +0100

    1

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').load()
+}
+
+
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+console.log(stripeSecretKey)
 
 const express = require('express')
 const app = express()
